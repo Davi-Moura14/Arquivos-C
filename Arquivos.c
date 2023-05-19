65cb9f77d252fa7d897955a3fd6a872e7164abf7
Davi-Moura14 <134111131+Davi-Moura14@users.noreply.github.com>
2023-05-19 20:52:44 -0300

    Add files via upload

---
+++ Arquivos.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+	FILE *arq;
+	char string[100], c;
+	int i;
+	arq = fopen("arquivo.txt","w");
+	if(arq == NULL){
+		printf("Erro na abertura do arquivo");
+		system("pause");
+		exit(1);
+	}
+	
+	printf("Entre com a string a ser gravada no arquivo:");
+	gets(string);
+	
+	
+	//Grava a string, caractere a caractere
+	
+	
+	for(i = 0; i < strlen(string); i++)
+		fputc(string[i], arq);
+		fclose(arq);
+		system("pause");
+		
+	arq = fopen("arquivo.txt","r");
+	if(arq == NULL){
+		printf("Erro na abertura do arquivo");
+		exit;
+	}
+	printf("Conteudo de arquivo.txt: ");
+	while(!feof(arq)){
+	    c = fgetc(arq);
+	    printf("%c",c);
+	}
+	fclose(arq);
+	return 0;
+}
